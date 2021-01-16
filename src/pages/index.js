3fac85a7f56207f61338301f77cbba10178a415f
Thanwar <thawardas25@gmail.com>
2021-01-16 19:38:31 +0500

    final version

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,11 +14,11 @@ const BookMarksQuery = gql`
 `
 
 const AddBookMarkMutation = gql`
-  mutation addBookmark($url: String!, $desc: String!){
-    addBookmark(url: $url, desc: $desc){
-     url 
+  mutation addBookmark($url: String!, $desc: String!) {
+    addBookmark(url: $url, desc: $desc) {
+      url
     }
-}
+  }
 `
 
 export default function Home() {
@@ -32,38 +32,54 @@ export default function Home() {
         url: textfield.value,
         desc: desc.value,
       },
-      refetchQueries: [{query:BookMarksQuery}],
+      refetchQueries: [{ query: BookMarksQuery }],
     })
     console.log("textfield", textfield.value)
     console.log("Desc", desc.value)
-    textfield.value=""
-    desc.value=""
-
+    textfield.value = ""
+    desc.value = ""
   }
 
   return (
     <div className="maincontainer">
-      <h1> Thanwar Bookmarking App </h1>
-      <br />
-      {/* <p>{JSON.stringify(data)}</p> */}
-      <ul>{data && data.bookmark.map(mark =>(
-        <li  key={mark.id}>
-          {mark.url} {"  |  "} {mark.desc}
-        </li>
-      ))}</ul>
-      
-      <br />
-      <br />
+        <div className="boxcontainer">
+        <h1 className="mainheading"> Thanwar Bookmarking App </h1>
+          <div className="box">
+            <div>
+          <input
+            className="inputs"
+            type="text"
+            placeholder="URL"
+            ref={node => (textfield = node)}
+          />
+          <br />
+          <input
+            className="inputs"
+            type="text"
+            placeholder="Description"
+            ref={node => (desc = node)}
+          />
+          <br />
+          <button className="button" onClick={addBookmarkSubmit}>
+            Bookmark
+          </button>
+          <br />
+          </div>
+          </div>
+        
 
-      <div className="secondcontainer">
-        <h1>Add Bookmarks</h1>
-        <input className="inputs" type="text" placeholder="URL"         ref={node => (textfield = node)} />
-      <br />
-        <input className="inputs" type="text" placeholder="Description" ref={node => (desc = node)} />
-      <br />
-        <button className="button" onClick={addBookmarkSubmit}>Bookmark</button>
-      <br />
+        <div className="bookmarks">
+          <ul>
+            {data &&
+              data.bookmark.map(mark => (
+                <li key={mark.id}>
+                  <h3 className="desc">{mark.desc}</h3>{" "}
+                  <a className="link" href={mark.url} target="_blank">{mark.url}</a>
+                </li>
+              ))}
+          </ul>
+          </div>
+        </div>
       </div>
-    </div>
   )
 }
